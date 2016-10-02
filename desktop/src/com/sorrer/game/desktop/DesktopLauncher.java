d0c3bad3ef0f51fd2f99c15429098be698788fe1
Sorrer <sorrer@gmail.com>
2016-10-01 22:34:32 -0400

    Config

--- desktop/src/com/sorrer/game/desktop/DesktopLauncher.java
+++ desktop/src/com/sorrer/game/desktop/DesktopLauncher.java
@@ -5,8 +5,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.sorrer.game.CoreGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int width = (int) (gd.getDisplayMode().getWidth() * .75f);
+		int height = (int) (gd.getDisplayMode().getHeight() * .75f);
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = width;
+		config.height = height;
+		config.resizable = true;
 		new LwjglApplication(new CoreGame(), config);
 	}
 }
