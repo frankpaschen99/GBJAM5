1a16305beedf8eb9108a206ca8fd7417fe564800
Frank Paschen <frankpaschen99@gmail.com>
2016-10-02 23:02:44 -0400

    gonna actually start working tmrw

---
+++ core/src/com/sorrer/game/entities/Player.java
@@ -0,0 +1,57 @@
+package com.sorrer.game.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.sorrer.utils.entity.Entity;
+
+import box2dLight.RayHandler;
+
+public class Player extends Entity {
+
+	public Player() {
+		System.out.println("Called");
+	}
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw(SpriteBatch b, ShapeRenderer sr) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addLights(RayHandler rayH) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Vector2 getPos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPos(float x, float y) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Vector2 getSize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
